a103a3607dcae1c97750fcfc01305f70a65a2e88
Kevin Yang Lu <luy@shinetechchina.com>
2015-10-08 14:06:41 +0800

    test

--- src/main/java/com/betalife/sushibuffet/controller/CorsFilter.java
+++ src/main/java/com/betalife/sushibuffet/controller/CorsFilter.java
@@ -6,20 +6,17 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class CorsFilter extends OncePerRequestFilter {
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request,
-			HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+			FilterChain filterChain) throws ServletException, IOException {
 		response.addHeader("Access-Control-Allow-Origin", "*");
-		System.out.println("++CorsFilter+++++============++++++++++++++");
-		response.addHeader("Access-Control-Allow-Methods",
-				"GET, POST, PUT, DELETE");
-		response.addHeader("Access-Control-Allow-Headers",
-				"X-Requested-With,Origin,Content-Type, Accept");
+		response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+		response.addHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type, Accept");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		filterChain.doFilter(request, response);
 	}
