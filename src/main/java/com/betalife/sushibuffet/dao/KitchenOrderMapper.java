af456d7ecdea3ce37606b37be06af8be849d1558
Kevin Yang Lu <luy@shinetechchina.com>
2015-12-17 14:40:51 +0800

    修改

---
+++ src/main/java/com/betalife/sushibuffet/dao/KitchenOrderMapper.java
@@ -0,0 +1,15 @@
+package com.betalife.sushibuffet.dao;
+
+import java.util.List;
+
+import com.betalife.sushibuffet.model.Order;
+
+public interface KitchenOrderMapper {
+
+	List<Order> selectColdDishes();
+
+	List<Order> selectHotDishes();
+
+	List<Order> selectServedDishes();
+
+}
