b199d06214b26184150447fe614eafad8187239d
luyang <lu.yang.end@gmail.com>
2016-02-10 00:33:33 +0800

    修改

--- src/main/java/com/betalife/sushibuffet/dao/DiningtableMapper.java
+++ src/main/java/com/betalife/sushibuffet/dao/DiningtableMapper.java
@@ -2,8 +2,10 @@ package com.betalife.sushibuffet.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.betalife.sushibuffet.model.Diningtable;
 
 public interface DiningtableMapper {
-	List<Diningtable> selectTables(Integer tableId);
+	List<Diningtable> selectTables(@Param(value = "id") Integer tableId);
 }
